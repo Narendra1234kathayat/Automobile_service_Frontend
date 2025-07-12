1d3afcdd679b90bd3541ba382dd089e0973b8e90
Narendra1234kathayat <nskathayat77@gmail.com>
2025-07-12 13:14:00 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css';
 import './index.css';
 import Navbar from './components/navbar/Navbar';
+import Login from './components/auth/Login';
 
 function App() {
   
@@ -11,42 +12,8 @@ function App() {
     <>
       <div className=' position-relative mainbox w-100'>
         <Navbar/>
-        <div className='container-fluid container-lg'>
-          <h1 className='text-center text-white mt-5'>Welcome to Automobile Service</h1>
-          <p className='text-center text-white'>Your one-stop solution for all automobile needs</p> 
-          </div>
-        <div className=' '>
-          <footer className='text-center text-white py-3'>
-            &copy; 2023 Automobile Service. All rights reserved.
-          </footer> 
-      </div>
-      <div className='container-fluid container-lg'>
-          <h1 className='text-center text-white mt-5'>Welcome to Automobile Service</h1>
-          <p className='text-center text-white'>Your one-stop solution for all automobile needs</p> 
-          </div>
-        <div className=' '>
-          <footer className='text-center text-white py-3'>
-            &copy; 2023 Automobile Service. All rights reserved.
-          </footer> 
-      </div>
-      <div className='container-fluid container-lg'>
-          <h1 className='text-center text-white mt-5'>Welcome to Automobile Service</h1>
-          <p className='text-center text-white'>Your one-stop solution for all automobile needs</p> 
-          </div>
-        <div className=' '>
-          <footer className='text-center text-white py-3'>
-            &copy; 2023 Automobile Service. All rights reserved.
-          </footer> 
-      </div>
-      <div className='container-fluid container-lg'>
-          <h1 className='text-center text-white mt-5'>Welcome to Automobile Service</h1>
-          <p className='text-center text-white'>Your one-stop solution for all automobile needs</p> 
-          </div>
-        <div className=' '>
-          <footer className='text-center text-white py-3'>
-            &copy; 2023 Automobile Service. All rights reserved.
-          </footer> 
-      </div>
+        <Login/>
+       
       
       </div>
     </>
